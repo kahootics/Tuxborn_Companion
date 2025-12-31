97dadb4db1f0a1d217ea3126b40efb71e102a3c6
kahootics <araldo.del.kaos@gmail.com>
2025-12-31 20:13:12 +0100

    uploading companion alpha version

---
+++ Companion/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://kahootics.github.io',
+    base: '/Tuxborn_Companion/'
+});
